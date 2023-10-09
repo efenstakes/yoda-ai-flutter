678acbba215aba40335dd2022d623dbf481791ad
Efen Stakes <efenstakes101@gmail.com>
2023-10-09 22:15:36 +0300

    if user has visited onboarding, navigate to homepage else go to onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:yoda_fl/providers/prompt.dart';
 import 'package:yoda_fl/screens/home/screen.dart';
+import 'package:yoda_fl/screens/onboarding/screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool? hasOnboarded = prefs.getBool("HAS::ONBOARDED");
+  runApp(MyApp(hasOnboarded: hasOnboarded ?? false));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // ignore: prefer_typing_uninitialized_variables
+  final hasOnboarded;
+  const MyApp({super.key, required this.hasOnboarded});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return MultiProvider(
       providers: [
 
@@ -28,7 +37,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const HomeScreen()
+        home: hasOnboarded ? const HomeScreen() : const OnboardingScreen()
       ),
     );
   }
