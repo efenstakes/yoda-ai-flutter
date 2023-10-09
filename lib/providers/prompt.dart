787f933f28f301ebe6a2eb47568f8ad31cf441b7
Efen Stakes <efenstakes101@gmail.com>
2023-10-09 22:11:03 +0300

    add loading & error states & use prompt service

--- lib/providers/prompt.dart
+++ lib/providers/prompt.dart
@@ -1,50 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:yoda_fl/models/prompt.dart';
+import 'package:yoda_fl/services/prompt.dart';
 
 
+enum ErrorType { outOfTokens, server }
 class PromptProvider with ChangeNotifier {
   bool isLoading = false;
-  bool hasError = false;
-  List<Prompt> prompts = [
-    Prompt(
-      prompt: "Hola",
-      reply: "Hola"
-    ),
-    Prompt(
-      prompt: "How are you doing",
-      reply: "Just ask something"
-    ),
-    Prompt(
-      prompt: "Hola",
-      reply: "Hola"
-    ),
-    Prompt(
-      prompt: "Hola",
-      reply: "Hola"
-    ),
-    Prompt(
-      prompt: "Hola",
-      reply: "Hola"
-    ),
-  ];
+  ErrorType? error;
+  List<Prompt> prompts = [];
   
 
   // submit prompt
   Future<void> submitPrompt({required String prompt}) async {
     isLoading = true;
+    error = null;
 
     try {
       // make api request to our api
-      String reply = "Reply to $prompt";
+      // String reply = "Reply to $prompt";
+      Prompt? promptReply = await PromptService.prompt(prompt);
       
+      // if data is null, we have an error
+      if( promptReply == null ) {
+
+        isLoading = false;
+        error = ErrorType.outOfTokens;
+        notifyListeners();
+        return;
+      }
+
       // add it to state
-      prompts.add(Prompt( prompt: prompt, reply: reply ));
+      prompts.add(promptReply);
+      isLoading = false;
     
     } catch (e) {
       
+      error = ErrorType.server;
+      print("error getting prompt");
+      print(e);
     }
 
-    isLoading = false;
     notifyListeners();
   }
   
